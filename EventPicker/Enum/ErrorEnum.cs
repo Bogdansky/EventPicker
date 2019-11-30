ca9fc8de587471262bbef37c4ecfa1148a6e06f4
Bogdansky <bogdya2@gmail.com>
2019-11-30 18:09:58 +0300

    Add error enum

---
+++ EventPicker/Enum/ErrorEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventPicker.Enum
+{
+    public enum ErrorEnum
+    {
+        BadRequest = 400, Unauthorized, Forbidden = 403,
+        NotFound, Conflict = 409,
+
+        InternalServerError = 500
+    }
+}
