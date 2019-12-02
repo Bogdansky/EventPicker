f22cbffe87a4a186107ccbc19c02ee640172534a
Bogdansky <bogdya2@gmail.com>
2019-12-02 21:52:31 +0300

    Add mark service

---
+++ BLL/Services/MarkService.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.DTO;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace BLL.Services
+{
+    public class MarkService : IService<MarkDTO>
+    {
+        Context Context { get; set; }
+
+        readonly IMapper mapper;
+
+        public MarkService()
+        {
+            Context = new Context();
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<DAL.Entities.Mark, MarkDTO>()
+                .ForMember(x => x.User, o => o.MapFrom(m => new UserDTO
+                {
+                    Id = m.User.Id,
+                    Login = m.User.Login,
+                    Nickname = m.User.Nickname
+                }))
+                .ForMember(x => x.Categories, o => o.MapFrom(m => m.Categories.Select(c => c.Id.ToString() + "_" + c.Name).Aggregate((x, y) => x + y))));
+            mapper = config.CreateMapper();
+        }
+
+        public async Task<MarkDTO> Create(int userId, CoordinatesDTO obj)
+        {
+            try
+            {
+                var isExist = await Context.Marks.CountAsync(m => obj.Equals(m.Coordinates)) > 0;
+                if (isExist)
+                {
+                    return null;
+                }
+                var user = await Context.Users.FindAsync(userId);
+                if (user == null)
+                {
+                    return new MarkDTO
+                    {
+                        Id = -1
+                    };
+                }
+                var mark = await Context.Marks.AddAsync(new DAL.Entities.Mark
+                {
+                    Coordinates = new DAL.Entities.Coordinates
+                    {
+                        Latitude = obj.Latitude,
+                        Longitude = obj.Longitude
+                    },
+                    User = user
+                });
+                var result = await Context.SaveChangesAsync();
+                return result == 0 ? null : new MarkDTO
+                {
+                    Id = mark.Entity.Id
+                };
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<MarkDTO> Delete(int markId, int userId)
+        {
+            var mark = await Context.Marks.Where(m => m.Id == markId && m.UserId == userId).FirstOrDefaultAsync();
+            if (mark == null)
+            {
+                return null;
+            }
+            _ = Context.Remove(mark);
+            return await Context.SaveChangesAsync() > 0 ? new MarkDTO() : null;
+        }
+
+        public Task<MarkDTO> Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<MarkDTO>> ReadAll()
+        {
+            try
+            {
+                var list = await Context.Marks.Include(m => m.Categories).Include(m => m.User).AsNoTracking().ToListAsync();
+                var result = list.Select(m => new MarkDTO
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Description = m.Description,
+                    ImageUrl = m.ImageUrl,
+                    User = new UserDTO
+                    {
+                        Id = m.User.Id,
+                        Nickname = m.User.Nickname
+                    },
+                    Categories = m.Categories == null || m.Categories.Count == 0 ? null : m.Categories?.Select(c => c.Id.ToString() + "_" + c.Name).Aggregate((x, y) => x + y),
+                    Coordinates = new CoordinatesDTO
+                    {
+                        Latitude = m.Coordinates.Latitude,
+                        Longitude = m.Coordinates.Longitude
+                    },
+
+                }).ToList();
+                return result;
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<MarkDTO>> ReadAll(int id)
+        {
+            try
+            {
+                var array = await Context.Marks.Where(m => m.UserId == id).Include(m => m.Categories).Include(m => m.Coordinates).ToArrayAsync();
+                //return mapper.Map<DAL.Entities.Mark[], MarkDTO[]>(array);
+                return array.Select(m => new MarkDTO
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Description = m.Description,
+                    ImageUrl = m.ImageUrl,
+                    Categories = m.Categories == null || m.Categories.Count == 0 ? null : m.Categories?.Select(c => c.Id.ToString() + "_" + c.Name).Aggregate((x, y) => x + y),
+                    Coordinates = new CoordinatesDTO
+                    {
+                        Latitude = m.Coordinates.Latitude,
+                        Longitude = m.Coordinates.Longitude
+                    },
+                    UserId = id
+                });
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<int> Update(int id, MarkDTO obj)
+        {
+            var mark = await Context.Marks.FindAsync(id);
+            if(mark == null)
+            {
+                return -1;
+            }
+            mark.Title = obj.Title ?? mark.Title;
+            mark.Description = obj.Description ?? mark.Description;
+            mark.ImageUrl = obj.ImageUrl ?? mark.ImageUrl;
+            
+            return await Context.SaveChangesAsync();
+        }
+
+        public Task<MarkDTO> Create(MarkDTO obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MarkDTO> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BLL/Services/UserService.cs
+++ BLL/Services/UserService.cs
@@ -75,7 +75,7 @@ namespace BLL.Services
 
         async public Task<UserDTO> Delete(int id)
         {
-            var user = await Context.Users.Include(u => u.UserInfo).Where(u => u.Id == id).FirstOrDefaultAsync();
+            var user = await Context.Users.Include(u => u.Marks).Where(u => u.Id == id).FirstOrDefaultAsync();
             if (user == null)
             {
                 return null;
@@ -87,7 +87,7 @@ namespace BLL.Services
 
         async public Task<UserDTO> Read(int id)
         {
-            var user = await Context.Users.Include(u => u.UserInfo).FirstOrDefaultAsync(u => u.Id == id);
+            var user = await Context.Users.Include(u => u.Marks).FirstOrDefaultAsync(u => u.Id == id);
             return user == null ? null : new UserDTO
             {
                 Id = user.Id,
@@ -98,7 +98,7 @@ namespace BLL.Services
 
         async public Task<UserDTO> Read(string email, string password)
         {
-            var user = await Context.Users.Include(u => u.UserInfo).FirstOrDefaultAsync(u => u.Login.Equals(email) && u.Verify(password));
+            var user = await Context.Users.Include(u => u.Marks).FirstOrDefaultAsync(u => u.Login.Equals(email) && u.Verify(password));
             return user == null ? null : new UserDTO
             {
                 Id = user.Id,
@@ -109,11 +109,10 @@ namespace BLL.Services
 
         async public Task<IEnumerable<UserDTO>> ReadAll()
         {
-            return await Context.Users.Include(u => u.UserInfo).Select(u => new UserDTO
+            return await Context.Users.Select(u => new UserDTO
             {
                 Id = u.Id,
-                Login = u.Login,
-                Password = null
+                Login = u.Login
             }).ToArrayAsync();
         }
 
@@ -134,61 +133,6 @@ namespace BLL.Services
         Task<UserDTO> IService<UserDTO>.Create(UserDTO obj)
         {
             throw new NotImplementedException();
-        }
-
-        public async void CheckTasks(int userId)
-        {
-            try
-            {
-                var user = Context.Users.Include(u => u.Progress).FirstOrDefault(u => u.Id == userId);
-                List<Progress> lostProgress = Context.Progress.Include(p => p.Task).Where(p => p.UserId == userId && !p.Done && p.Day < DateTime.Now).ToList();
-                if (lostProgress.Count() == 0)
-                {
-                    return;
-                }
-                var idSequence = lostProgress.Distinct(new ProgressComparer()).Select(p => p.BookId).ToList();
-                var books = Context.Books.Where(b => idSequence.Contains(b.Id)).ToList();
-                string message = GetHtml(lostProgress, books);
-            }
-            catch
-            {
-
-            }
-        }
-
-        private string GetHtml(List<Progress> progress, List<Book> books)
-        {
-            StringBuilder html = new StringBuilder("Dear user! You have missed tasks<br/>");
-            foreach (var book in books)
-            {
-                StringBuilder bookHtml = new StringBuilder($"<h3>{book.Author} \"{book.Name}\"({book.NumberOfPages} pages)</h3><br/>")
-                                            .Append("<table border='1'>")
-                                            .Append("<thead>")
-                                            .Append("<th>Date</th><th>Pages</th>")
-                                            .Append("</thead><tbody>");
-                foreach (var p in progress.Where(pr => pr.BookId == book.Id))
-                {
-                    bookHtml = bookHtml.Append("<tr>")
-                                       .AppendFormat("<td>{0}</td><td>{1}</td>", p.Day.ToShortDateString(), p.Task.Pages)
-                                       .Append("</tr>");
-                }
-                bookHtml = bookHtml.Append("</tbody></table><br/>");
-                html = html.Append(bookHtml, 0, bookHtml.Length);
-            }
-            return html.Append("<h2>We hope you're alright. Good luck!</h2>").ToString();
-        }
-    }
-
-    internal class ProgressComparer : EqualityComparer<Progress>
-    {
-        public override bool Equals(Progress x, Progress y)
-        {
-            return x.BookId == y.BookId;
-        }
-
-        public override int GetHashCode(Progress obj)
-        {
-            return obj == null ? 0 : obj.BookId.GetHashCode();
         }
     }
 }
